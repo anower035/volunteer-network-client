28ed3cbb256b58673f28f87684b702d3d096175b
anower035 <faysalanower71@gmail.com>
2021-01-27 22:10:46 +0600

    new update for volunteer

--- src/component/Registration/Registration.js
+++ src/component/Registration/Registration.js
@@ -11,6 +11,33 @@ import { Link, useParams } from 'react-router-dom';
 import { UserContext } from '../../App';
 import { Container, Form, FormControl } from 'react-bootstrap';
 import './Registration.css';
+import { makeStyles } from '@material-ui/core';
+
+
+
+
+const useStyles = makeStyles({
+    root: {
+      "& .MuiInputBase-root": {
+        padding: 0,
+        "& .MuiButtonBase-root": {
+          padding: 0,
+          paddingLeft: 10,
+          marginRight: 15,
+        },
+        "& .MuiInputBase-input": {
+          padding: 15,
+          paddingLeft:10,
+          paddingRight:96,
+          border:"none"
+        },
+        "& .MuiInputBase-input : focus": {
+            borderColor:'none'
+        }
+      }
+    }
+  });
+
 
 const Registration = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
@@ -18,6 +45,8 @@ const Registration = () => {
     const [selectedDate, setSelectedDate] = useState({
         datefill : new Date()
     });
+    
+    const classes = useStyles();
 
     const handleDateChange = (date) => {
         const newDates ={...selectedDate}
@@ -55,32 +84,39 @@ const Registration = () => {
                         <div>
                             <FormControl name="name" type="text" placeholder="Full Name" defaultValue={loggedInUser.name} className="my-3 bg-light" required />
                             <FormControl name="name" type="text" placeholder="Email" defaultValue={loggedInUser.email} className="my-3 bg-light" required />
-                            <Form className="bg-light" required>
-                                <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                                    <Grid container justify="center">
-                                    <KeyboardDatePicker
-                                        inputVariant="outlined"
-                                        id="date-picker-dialog"
-                                        label="Date"
-                                        format="dd/MM/yyyy"
-                                        defaultValue={selectedDate.datefill}
-                                        InputProps={{
-                                            disableUnderline: true,
-                                           }}
-                                        onChange={() => handleDateChange()}
-                                        KeyboardButtonProps={{
-                                            'aria-label': 'change date',
-                                        }}
+                            <Form className="my-3 bg-light dated" required>
+
+                                    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                                        <Grid container justify="space-around">
+                                        <KeyboardDatePicker
+                                            className={classes.root}
+                                            autoOk
+                                            variant="inline"
+                                            inputVariant="outlined"
+                                            label="Date"
+                                            format="MM/dd/yyyy"
+                                            value={selectedDate.datefill}
+                                            InputAdornmentProps=
+                                                {{ 
+                                                    position: "end",
+                                                    underline:"false",
+                                                    
+                                                }}
+                                            onChange={handleDateChange}
+                                            KeyboardButtonProps={{
+                                                "aria-label":"change-date"
+                                            }}
                                         />
-                                    </Grid>
-                                </MuiPickersUtilsProvider>
+                                        </Grid>
+                                    </MuiPickersUtilsProvider>
+
                             </Form>
                             <FormControl id="description" name="name" type="text" placeholder="Description" className="my-3 bg-light" required />
                             <FormControl name="name" type="text" placeholder="Organize books at the library" defaultValue={volunteerItem} className="my-3 bg-light" required /> 
                         </div>
-                        <Link to="/registrationList">
-                            <button onClick={handleRegistration} variant="light" className="btn btn-primary my-3 d-block w-100 p-1 regButton">
-                                <h6>Registration</h6>
+                        <Link to="/registrationList" style={{ textDecoration: 'none'}}>
+                            <button onClick={handleRegistration} variant="light" className="btn btn-primary my-3 mt-5 d-block w-100 p-1 regButton">
+                                <h6  className="pt-1">Registration</h6>
                             </button>
                         </Link>
                     </Form>
